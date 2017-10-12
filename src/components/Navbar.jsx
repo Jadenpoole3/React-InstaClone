043d93d15070627c297f5d4d3881b89ef83df0ba
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-10-12 17:02:33 -0400

    insta clone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,48 @@ import React, { Component } from 'react';
 import { FaInstagram, FaUser, FaHeartO, FaCompass, FaSearch } from 'react-icons/lib/fa';
 import styled from 'styled-components';
 
+// const FlexContainer = styled.div`
+// display: flex;
+// flex-direction: row;
+// `;
+
+// const NavbarWrapper = FlexRow.extend`
+// justify-content: space-between;
+// align-items: center;
+// padding: 26px 5vw;
+// width: 90vw;
+// height: 25px;
+
+// `
+
+ const Header = styled.div`
+ background-color: white;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+    padding: 26px 5vw;
+    width: 90vw;
+    height: 25px;
+ `;
+ 
+
 class Navbar extends Component{
   render(){
     return (
+      <Header>
+ <h3><FaInstagram /> | Instaclone</h3>
+ 
       <div>
-        <h3><FaInstagram /> | Instaclone</h3>
-        <FaSearch/><input type="text"/>
+        <FaSearch/>
+        <input type="text"/>
+      </div>
+        
         <div>
           <FaCompass />
           <FaHeartO />
           <FaUser />
         </div>
-      </div>
+      </Header>
     );
   }
 }
